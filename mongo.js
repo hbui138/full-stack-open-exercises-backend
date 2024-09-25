f16d86d47037bdbbf9b126c9d6979657be0edcc9
BuiDaiHieu <91104220+BuiDaiHieu@users.noreply.github.com>
2024-09-25 16:04:50 +0300

    eslint fix

--- mongo.js
+++ mongo.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length<3) {
-    console.log('give password as argument')
-    process.exit(1)
+  console.log('give password as argument')
+  process.exit(1)
 }
-  
+
 const password = process.argv[2]
 
 const url = `mongodb+srv://buidaihieu2004:${password}@phonebookapp.sqatybq.mongodb.net/Phon?retryWrites=true&w=majority&appName=PhonebookApp`
@@ -14,34 +14,34 @@ mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String
+  name: String,
+  number: String
 })
 
 const Person = mongoose.model('Person', personSchema)
 
 
 if (process.argv.length === 3) {
-    // Show all persons
-    Person.find({}).then(result => {
-        console.log('phonebook:');
-        result.forEach(person => {
-            console.log(`${person.name} ${person.number}`);
-        });
-        mongoose.connection.close();
-    });
+  // Show all persons
+  Person.find({}).then(result => {
+    console.log('phonebook:')
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
+    })
+    mongoose.connection.close()
+  })
 } else if (process.argv.length === 5) {
-    // Add a new person
-    const newPerson = new Person({
-        name: process.argv[3] ,
-        number: process.argv[4]
-    });
+  // Add a new person
+  const newPerson = new Person({
+    name: process.argv[3] ,
+    number: process.argv[4]
+  })
 
-    newPerson.save().then(result => {
-        console.log(`added ${newPerson.name} number ${newPerson.number} to phonebook`);
-        mongoose.connection.close();
-    });
+  newPerson.save().then(() => {
+    console.log(`added ${newPerson.name} number ${newPerson.number} to phonebook`)
+    mongoose.connection.close()
+  })
 } else {
-    console.log('incorrect number of arguments');
-    mongoose.connection.close();
+  console.log('incorrect number of arguments')
+  mongoose.connection.close()
 }
